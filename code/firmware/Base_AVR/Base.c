313a9d023526323c2eeb12a24330532e6d9b22bc
Danilo Pena <danilopena.ba@gmail.com>
2019-09-08 15:23:58 -0300

    Update firmware

--- code/firmware/Base_AVR/Base.c
+++ code/firmware/Base_AVR/Base.c
@@ -38,7 +38,13 @@ int main(void){
 	SREG = 1<<SREG_I;
 	//CLKPR |= 1<<CLKPCE;
 	//CLKPR = 0x04;
-	cont = 0;
+	cont = 8;
+	addr = 0;
+	totalReg = 0xFF;
+	tipoEnvio = 0; // 0- Envia frame para UART_TX ;1- Envia dados da EEPROM para UART_TX
+	eeprom_write_byte((uint8_t*)addr,totalReg); // 0xFF(addr0x00)
+	flagEeprom = 0;
+	contador = 0;
 
 	frame[0] = 0x7E; //7E
 	frame[1] = 0x00; //00
@@ -50,7 +56,7 @@ int main(void){
 	frame[7] = 0x0D; //0D
 
 	// Button
-    PCICR = 1<<PCIE0; // Habilitando interrupção do PORTB
+	PCICR = 1<<PCIE0; // Habilitando interrupção do PORTB
 	PCMSK0 = (1<<PCINT0)|(1<<PCINT1)|(1<<PCINT2); // Definindo pinos de interrupção PCINT1 (B0)
 
 	// UART0
@@ -61,13 +67,46 @@ int main(void){
 	UBRR0H = (BAUD>>8);
 	UCSR0B |= (1<<RXCIE0); // Habilita recepção
 
+	// Timer0
+	TIMSK0 &= ~(1<<TOIE0);
+	TCCR0B = 0x05; // Prescaller 1024
+
+	// EEPROM
+	//uint8_t ByteOfData;
+
 	// Loop
-	while(1);
+	while(1){/*
+		if(cont<8){
+			UCSR0B |= (1<<TXEN0)|(1<<TXCIE0);
+		}else{
+			UCSR0B &= ~(1<<TXCIE0);
+		}*/
+
+		if((UCSR0A&FE0)|(UCSR0A&DOR0)|(UCSR0A&UPE0)){ // Tratamento de erro de UART
+			PORTB |= LED4;
+		}
+		if(flagEeprom==1){
+			TIMSK0 |= (1<<TXEN0)|(1<<TOIE0);
+		}
+	};
 }
 
 // Interrupção externa
 ISR(PCINT0_vect){
-	if(PINB&0x04){ // Button3 Envia dados para xBee
+	if(PINB&0x01){ // Button1 Envia dados para PC
+		if(addr>0){
+			UCSR0B |= (1<<TXEN0)|(1<<TXCIE0);
+			PORTB |= LED1;
+			PORTB &= ~LED3;
+			tipoEnvio = 0;
+		}
+	}
+	else if(PINB&0x02){ // Button2 Desliga a UART
+		UCSR0B = 0x00;
+		PORTB |= LED3;
+	}
+	else if(PINB&0x04){ // Button3 Envia dados para xBee
+		tipoEnvio = 1;
 		cont=0;
 		UCSR0B |= (1<<TXEN0)|(1<<TXCIE0);
 		PORTB |= LED3;
@@ -76,9 +115,59 @@ ISR(PCINT0_vect){
 
 // Interrupção UART TX
 ISR(USART_TX_vect){
-	UDR0 = frame[cont];
-	cont++;
-	if(cont>7){
-		UCSR0B &= ~(1<<TXCIE0);
+	if(tipoEnvio == 0){
+		UDR0 = eeprom_read_byte((uint8_t*)(totalReg - addr));
+		//UDR0 = frameEeprom[totalReg-addr];
+		if(addr==0){
+			UCSR0B &= ~(1<<TXCIE0);
+			PORTB &= ~LED2;
+		}
+		else{
+			addr--;
+		}
+	}
+	else if(tipoEnvio == 1){
+		UDR0 = frame[cont];
+		cont++;
+		if(cont>7){
+			UCSR0B &= ~(1<<TXCIE0);
+		}
 	}
 }
+
+// Interrupção UART RX
+ISR(USART_RX_vect){
+	resposta = UDR0;
+	addr++;
+	frameEeprom[addr] = resposta;
+	//eeprom_write_byte((uint8_t*)addr,resposta);
+	totalReg = addr;
+	frameEeprom[0] = totalReg;
+	eeprom_write_byte((uint8_t*)0,totalReg);
+	PORTB |= LED2;
+	flagEeprom=1; // Salvar na EEPROM
+}
+
+// Armazena dados na EEPROM
+void save_eeprom(){
+	eeprom_write_byte((uint8_t*)(totalReg-addr),frameEeprom[totalReg-addr]);
+	addr--;
+	if(addr<1){
+		eeprom_write_byte((uint8_t*)(totalReg),frameEeprom[totalReg]);
+		flagEeprom = 0; // Acabou de armazenar na EEPROM
+		TIMSK0 &= ~(1<<TOIE0);
+		addr = totalReg;
+	}
+}
+
+// Interrupção de Timer0
+ISR(TIMER0_OVF_vect){
+	if(contador<50){
+		contador++;
+	}
+	else{
+		save_eeprom();
+		contador = 0;
+	}
+}
+
